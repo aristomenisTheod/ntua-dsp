a799bc5b20d86e2cca6fe16f41cfc759b13b8d21
LoniasGR <avdelasleonidas@gmail.com>
2019-03-23 13:31:36 +0200

    Music Analysis addded

--- Assignment 1/assignment1_3.m
+++ Assignment 1/assignment1_3.m
@@ -10,7 +10,6 @@ close all;
 t = length(speech)/Fs;
 tt = linspace(1, t, length(speech));
 win_len = 0.02 * Fs;
-% TODO: Make it a function
 energyST = hamm_ste(speech, win_len);
 
 delay = fix((win_len - 1)/2);
@@ -22,6 +21,25 @@ plot(tt, speech, tt(1:end-delay), energyST(delay+1:end));
 ZCR_norm = hamm_zcr(speech, win_len);
 figure();
 plot(tt, speech, tt(1:end-delay +1), ZCR_norm(delay+1:end));
+
+% Section 3.2
+[music, Fs] = audioread('music.wav');
+
+t = length(speech)/Fs;
+tt = linspace(1, t, length(music));
+win_len = 0.02 * Fs;
+energyST = hamm_ste(music, win_len);
+
+delay = fix((win_len - 1)/2);
+figure();
+plot(tt, music, tt(1:end-delay), energyST(delay+1:end));
+
+% ZCR
+
+ZCR_norm = hamm_zcr(music, win_len);
+figure();
+plot(tt, speech, tt(1:end-delay +1), ZCR_norm(delay+1:end));
+
 
 function energyST = hamm_ste (signal, win_len)
     win_overlap = win_len - 1;
