cf8f018acf45ed7eee2a33a0c6b569ad3b649667
arist <theodoridisaristomenis@gmail.com>
2019-04-29 16:52:45 +0300

    some changes

--- Assignment 2/assignment2_1.m
+++ Assignment 2/assignment2_1.m
@@ -5,8 +5,8 @@ close all;
 % Part 1
 % Section 1.0
 
-[music, Fs] = audioread('music-dsp19.wav');
-
+[music_stereo, Fs] = audioread('music-dsp19.wav');
+music = (music_stereo(:,1)+music_stereo(:,2))/2;
 music_norm = music/max(music);
 
 t = length(music_norm)/Fs;
@@ -14,10 +14,28 @@ tt = linspace(1, t, length(music_norm));
 figure();
 plot(tt, music_norm);
 
-win_len = 512/Fs;
-ham_win = hamming(win_len);
+win_len = 512;
+ham_win = hann(win_len);
 
 musicFramed = buffer(music_norm, win_len);
 diagWin = diag(sparse(ham_win));
-musicWindowed = musicFramed * diagWin;
+musicWindowed = diagWin * musicFramed;
 
+%tl = linspace(1, 512, 512);
+%figure();
+%plot(tl, musicWindowed(:,1));
+
+% Section 1.1
+
+musicPK = pk(musicWindowed);
+k = linspace(0, 256, 
+figure();
+plot(musicPK(:,2));
+
+function p = pk(signal)
+    sigFft = abs(fft(signal)).^2;
+    p = 90.302 + 10*log10(sigFft);
+end
+function b = bark(f)
+    b = 13*atan(0.00076*f)+3.5*atan((f/7500)^2);
+end
