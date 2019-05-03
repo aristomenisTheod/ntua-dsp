a851f34e77322b3ab1c139953850a9f313c0eeca
arist <theodoridisaristomenis@gmail.com>
2019-05-03 20:39:26 +0300

    more changes

--- Assignment 2/assignment2_2.m
+++ Assignment 2/assignment2_2.m
@@ -58,8 +58,8 @@ for win = 1 : size(undersampled_u_k,3)
     for k = 1 : size(undersampled_u_k,2)
         music_broken = unique(musicWindowedNotNormed(:,k));
         R = length(music_broken);
-        Xmin = min(music_broken);
-        Xmax = max(music_broken);
+        Xmin = min(musicWindowed2(:,k));
+        Xmax = max(musicWindowed2(:,k));
         Bk(k,1) = ceil(log2(R/min(Tg(:,k)))-1);
         D = (Xmax-Xmin)/(2.0^Bk(k,1));
         num_quant = 2^Bk(k,1);
@@ -144,19 +144,98 @@ for win = 1 : size(filtered,2)
         music_almost(:,win) = music_almost(:,win) + filtered(:, win, k);
     end
 end
+music_almost(:,1) = music_almost(:,1)/max(music_almost(:,1));
 figure();
 plot(music_almost(:,1));
 
-new_music = zeros((size(music_almost,1)-M+1)*size(music_almost,2),1);
-for win = 1 : size(music_almost,2)
-    indexing = (size(music_almost,1)-M+1)*(win);
-    new_music(indexing:indexing+(size(music_almost,1)-M+1),1) = music_almost(M-1:size(music_almost,1),win);
+new_music = zeros((size(music_almost,1)-2*L+1)*size(music_almost,2),1);
+indexing = size(music_almost,1);
+new_music(1:size(music_almost,1),1) = ...
+        new_music(1:(size(music_almost,1)),1)+music_almost(1:size(music_almost,1),win);
+for win = 2 : size(music_almost,2)
+    new_music(indexing-2*L:indexing+size(music_almost,1)-2*L-1,1) = ...
+        new_music(indexing-2*L:indexing+(size(music_almost,1))-2*L-1,1)+music_almost(1:size(music_almost,1),win);
+    indexing = indexing + size(music_almost,1)-L*2;
+end
+new_music = new_music/max(new_music);
+audiowrite('compressed.wav',new_music(1:indexing),44100);
+figure();
+plot(new_music(1:indexing,1));
+
+% Mean Square Error (MSE) %
+
+% err = immse(music, new_music(1:size(music,1),1));
+for i = 1: size(new_music,1)
+    if(new_music(i,1) > 0)
+        break;
+    end
+end
+err = MSE(music,new_music(i:size(music,1)+i-1,1));
+error = sum(err(:,1))/size(err(:,1),1);
+figure();
+plot(err(:,1));
+%% 8bit %%
+% Oversampling %
+
+oversampled_music8 = zeros(M*size(NonAdaptive_quantized,1),size(NonAdaptive_quantized,2), size(NonAdaptive_quantized,3));
+for win = 1 : size(oversampled_music8,3)
+    for k = 1 : size(oversampled_music8,2)
+        for i = 1 : size(NonAdaptive_quantized,1)
+            oversampled_music8(i*M,k,win) = NonAdaptive_quantized(i,k,win);
+        end
+    end
 end
 
-figure();
-plot(new_music(:,1));
+% Filtering %
 
+filterbank8 = zeros(L, M);
+
+for k = 1:M
+    for n = 1:L
+        filterbank8(n, k) = g_k(n, k, M);
+    end
+end
+
+filtered8 = zeros(size(oversampled_music8,1)+size(filterbank8,1)-1,size(oversampled_music8,2), size(oversampled_music8,3));
+for win = 1 : size(oversampled_music8,2)
+    for k = 1 : size(oversampled_music8,3)
+        filtered8(:, win, k) = conv(filterbank8(:,k), oversampled_music8(:,win,k)); 
+    end
+end
+
+% Final reconstruction %
+music_almost8 = zeros(size(filtered8,1),size(filtered8,2));
+for win = 1 : size(filtered8,2)
+    for k = 1 : size(filtered8,3)
+        music_almost8(:,win) = music_almost8(:,win) + filtered8(:, win, k);
+    end
+end
+figure();
+plot(music_almost8(:,1));
+
+new_music8 = zeros((size(music_almost8,1)-2*L+1)*size(music_almost8,2),1);
+indexing8 = size(music_almost8,1);
+new_music8(1:size(music_almost8,1),1) = ...
+        new_music8(1:(size(music_almost8,1)),1)+music_almost8(1:size(music_almost8,1),win);
+for win = 2 : size(music_almost8,2)
+    new_music8(indexing8-2*L:indexing8+size(music_almost8,1)-2*L-1,1) = ...
+        new_music8(indexing8-2*L:indexing8+(size(music_almost8,1))-2*L-1,1)+music_almost8(1:size(music_almost8,1),win);
+    indexing8 = indexing8 + size(music_almost8,1)-L*2;
+end
+new_music8 = new_music8/max(new_music8);
+audiowrite('compressed8.wav',new_music8(1:indexing8),44100);
+figure();
+plot(new_music8(1:indexing8,1));
+
+% Mean Square Error (MSE) %
+
+err8 = MSE(music,new_music8(i:size(music,1)+i-1,1));
+error8 = sum(err8(:,1))/size(err8(:,1),1);
 %% Functions %%
+function result = MSE(first, second)
+%     result = zeros(size(first,1),1);
+    result = (first-second).^2;
+end
 
 function result = g_k(n,k,M)
 %myFun - Description
